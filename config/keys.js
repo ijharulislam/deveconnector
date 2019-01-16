93da21cef6c3d0b937e921cf65fa06b0abf6d335
ijharulislam <ijharalkawsary@gmail.com>
2019-01-17 01:03:55 +0600

    Basic Node server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://ijhar:ijhar1234@ds159164.mlab.com:59164/ijhartest"
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDb Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hello nodemon!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
